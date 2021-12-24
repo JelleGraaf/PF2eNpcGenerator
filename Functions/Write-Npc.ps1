14c925aa7687926f89ac1dc5fba0d2e4b9fed06a
Jelle de Graaf <palagast@hotmail.com>
2021-12-24 14:28:25 +0100

    Revamp the end result to more final lay-out

--- Functions/Write-Npc.ps1
+++ Functions/Write-Npc.ps1
@@ -1,45 +1,40 @@
 function Write-NPC {
+    Clear-Host
     Write-Host
-    Write-Host "GRAND TOTAL:" -ForegroundColor Cyan
-    Write-Host "Ancestry: $($Ancestry.Name). Class: $Class ($Subclass). Level: $Level. Hit points: $Hp." -ForegroundColor Yellow
-    Write-Host "Stats: STR: $($Stats.Str), DEX: $($Stats.Dex), CON: $($Stats.Con), INT: $($Stats.Int), WIS: $($Stats.Wis), CHA: $($Stats.Cha)" -ForegroundColor Yellow
-    Write-Host "Background: $($Background.Name)." -ForegroundColor Yellow
-    Write-Host "Skills:" -ForegroundColor Yellow; Write-Skills
-    Write-Host "Languages:" -ForegroundColor Yellow; $Global:Languages
-    Write-Host "Saving throws: Fort: $FortSaveNumber. Ref: $RefSaveNumber. Will: $WillSaveNumber."
-    Write-Host "Class DC: $ClassDcNumber"
-    Write-Host "Special abilities:" -ForegroundColor Yellow; $Global:SpecialAbilities
-    Write-Host "Ancestry feats:" -ForegroundColor Yellow; $Global:AncestryFeats
-    Write-Host "Class feats:" -ForegroundColor Yellow; $Global:ClassFeats
-    Write-Host "General feats:" -ForegroundColor Yellow; $Global:GeneralFeats
-    Write-Host "Skill feats:" -ForegroundColor Yellow; $Global:SkillFeatsChosen
-        
-    
-    #Write-Host "THIS IS IT!" -ForegroundColor Red
-    #Write-Host "$Ancestry $Class                                                                  Humanoid $Level"
-    #Write-Host "Perception: $Perception."
-    #Write-Host "Languages: $Languages."
-    #Write-Host "Skills: $Skills"
-    #Write-Host "STR: $($Stats.Str), DEX: $($Stats.Dex), CON: $($Stats.Con), INT: $($Stats.Int), WIS: $($Stats.Wis), CHA: $($Stats.Cha)"
-    #Write-Host ""
+    Write-Host "+* NPC statistics *+" -ForegroundColor Magenta
+    Write-Host "$($Ancestry.Name) $Class ($Subclass)                                                Humanoid $Level"
+    Write-Host "Perception: $(Get-ProficiencyNumber -Proficiency $Global:Perception -BoundStat Wis)."
+    Write-Host "Languages: $Global:Languages."
+    Write-Host "Skills: " -NoNewline; Write-Skills
+    Write-Host "STR: $($Stats.Str), DEX: $($Stats.Dex), CON: $($Stats.Con), INT: $($Stats.Int), WIS: $($Stats.Wis), CHA: $($Stats.Cha)"
+    Write-Host ""
+    Write-Host "------------------------------------------------------------------------------------------------"
+    Write-Host "AC: <Not implemented yet>" -ForegroundColor DarkGray
+    Write-Host " Fort: $FortSaveNumber, Ref: $ReflexSaveNumber, Will: $WillSaveNumber"
+    Write-Host "HP: $Hp"
+    Write-Host "------------------------------------------------------------------------------------------------"
+    Write-Host "Speed: $Global:Speed"
+    Write-Host "Melee: <Not implemented yet>" -ForegroundColor Darkgray
+    Write-Host "Ranged: <Not implemented yet>" -ForegroundColor Darkgray
+    #Write-Host "Melee: $MeleeWeapon $MeleeToHit. Damage: $MeleeWeaponDamageTotal" # TODO
+    #Write-Host "Ranged: $RangedWeapon $RangedToHit. Damage: $RangedWeaponDamageTotal" # TODO
+    Write-Host "------------------------------------------------------------------------------------------------"
+    #Write-Host "Spells: $Spells" # TODO
     #Write-Host "------------------------------------------------------------------------------------------------"
-    #Write-Host "AC: $AC; Fort: $FortSave, Ref: $ReflexSave, Will: $WillSave"
-    #Write-Host "HP: $HitPoints"
-    #Write-Host ""
-    #Write-Host "------------------------------------------------------------------------------------------------"
-    #Write-Host "Speed: $Speed"
-    #Write-Host "Melee: $MeleeWeapon $MeleeToHit. Damage: $MeleeWeaponDamageTotal"
-    #Write-Host "Ranged: $RangedWeapon $RangedToHit. Damage: $RangedWeaponDamageTotal"
-    #Write-Host ""
-    #Write-Host "------------------------------------------------------------------------------------------------"
-    #Write-Host "Spells: $Spells"
-    #Write-Host ""
-    #Write-Host "------------------------------------------------------------------------------------------------"
-    #Write-Host "Background information"
-    #Write-Host "Background: $($Background.Name)."
-    #Write-Host "------------------------------------------------------------------------------------------------"
-    #Write-Host ""
-    #Write-Host ""
-    #Write-Host ""
-    #Write-Host ""
+    if ($Global:SpecialAbilities) {
+        Write-Host "Special abilities:" -ForegroundColor Yellow
+        Write-Host "$Global:SpecialAbilities"
+        Write-Host "------------------------------------------------------------------------------------------------"
+    }
+    Write-Host "Ancestry feats" -ForegroundColor Yellow; $Global:AncestryFeats
+    Write-Host "Class feats" -ForegroundColor Yellow; $Global:ClassFeats
+    Write-Host "General feats" -ForegroundColor Yellow; $Global:GeneralFeats
+    Write-Host "Skill feats" -ForegroundColor Yellow; $Global:SkillFeatsChosen
+    Write-Host "------------------------------------------------------------------------------------------------"
+    Write-Host "Background information"  -ForegroundColor Yellow
+    Write-Host "Background: $($Background.Name)."
+    Write-Host "Background story: <Not implemented yet>" -ForegroundColor Darkgray
+    Write-Host "------------------------------------------------------------------------------------------------"
+    Write-Host ""
+    Write-Host ""
 }
